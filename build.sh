5c8e9fc3f044997532ba1f6e8a8a1d0522bf8559
git-hulk <hulk.website@gmail.com>
2020-08-11 17:46:39 +0800

    MOD: remove build install flag

--- build.sh
+++ build.sh
@@ -58,7 +58,7 @@ VERSION=`grep "^VERSION" Changelog | head -1 | cut -d " " -f2`
 BUILD_DATE=`date -u +'%Y-%m-%dT%H:%M:%SZ'`
 GIT_REVISION=`git rev-parse --short HEAD`
 
-GOOS="$TARGET_OS" GOARCH="$TARGET_ARCH" go build -i -v -ldflags \
+GOOS="$TARGET_OS" GOARCH="$TARGET_ARCH" go build -v -ldflags \
     "-X $GO_PROJECT/version.Version=$VERSION -X $GO_PROJECT/version.BuildDate=$BUILD_DATE -X $GO_PROJECT/version.BuildCommit=$GIT_REVISION" \
     -o ${TARGET_NAME} ${GO_PROJECT}/server
 if [[ $? -ne 0 ]]; then

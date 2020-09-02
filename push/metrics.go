0881ac4596b2e637e20889c34b333d193eb4307d
李广博 <lgb@meitu.com>
2020-09-02 14:14:03 +0800

    ADD: pusher metrics

---
+++ push/metrics.go
@@ -0,0 +1,53 @@
+package push
+
+import "github.com/prometheus/client_golang/prometheus"
+
+type PerformanceMetrics struct {
+	ConsumeLatencies *prometheus.SummaryVec
+	PushLatencies    *prometheus.SummaryVec
+	PushHTTPCodes    *prometheus.CounterVec
+}
+
+var metrics *PerformanceMetrics
+
+func setupMetrics() {
+	metrics = &PerformanceMetrics{}
+	consumeLatencies := prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Namespace:  "infra",
+			Subsystem:  "lmstfy_pusher",
+			Name:       "consume_latency_milliseconds",
+			Help:       "latencies",
+			Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.95: 0.001},
+		},
+		[]string{"pool", "namespace", "queue"},
+	)
+
+	pushLatencies := prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Namespace:  "infra",
+			Subsystem:  "lmstfy_pusher",
+			Name:       "push_latency_milliseconds",
+			Help:       "push http latencies",
+			Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.95: 0.001},
+		},
+		[]string{"pool", "namespace", "queue"},
+	)
+
+	httpCodes := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: "infra",
+			Subsystem: "lmstfy_pusher",
+			Name:      "push_http_codes",
+			Help:      "push response code",
+		},
+		[]string{"pool", "namespace", "queue", "code"},
+	)
+
+	prometheus.MustRegister(consumeLatencies)
+	prometheus.MustRegister(pushLatencies)
+	prometheus.MustRegister(httpCodes)
+	metrics.ConsumeLatencies = consumeLatencies
+	metrics.PushLatencies = pushLatencies
+	metrics.PushHTTPCodes = httpCodes
+}

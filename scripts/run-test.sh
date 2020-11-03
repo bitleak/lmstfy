3a257702b45e36ffe785d0c37b6ab1530913ace2
git-hulk <hulk.website@gmail.com>
2020-11-03 11:22:36 +0800

    MOD: go test with code coverage mode

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -e -x
 
-go list ./... | grep -v client | xargs -n 1 go test -v
+#go list ./... | grep -v client | xargs -n 1 go test -v
+
+go list ./...|grep -v client|awk '{printf $1" "}'|xargs go test -v -covermode=count -coverprofile=coverage.out 
+

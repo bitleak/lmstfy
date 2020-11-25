6fd640014e5c965bf6dda4089a8129a00eb711a3
hulk <hulk.website@gmail.com>
2020-11-25 14:03:13 +0800

    FIX: `bt_func` and `bt_line` was went wrong after upgrading the vendor to mod (#87)

--- log/backtrack_test.go
+++ log/backtrack_test.go
@@ -1,10 +1,31 @@
 package log
 
 import (
+	"bytes"
+	"encoding/json"
+	"strings"
 	"testing"
 
 	"github.com/sirupsen/logrus"
 )
+
+func TestFire(t *testing.T) {
+	var buf bytes.Buffer
+	logger := logrus.New()
+	logger.SetFormatter(&logrus.JSONFormatter{})
+	logger.SetOutput(&buf)
+	logger.Hooks.Add(NewBackTrackHook(logrus.DebugLevel))
+	logger.SetLevel(logrus.DebugLevel)
+	logger.Error("test backtrace")
+	entry := make(map[string]string)
+	json.Unmarshal(buf.Bytes(), &entry)
+	if !strings.HasSuffix(entry["bt_func"], "TestFire") {
+		t.Fatal("bt_func suffix should be TestFunc was expected")
+	}
+	if !strings.Contains(entry["bt_line"], "backtrack_test.go") {
+		t.Fatal("bt_func suffix contains backtrack_test.go was expected")
+	}
+}
 
 func TestBackTrackFormatter(t *testing.T) {
 	logger := logrus.New()

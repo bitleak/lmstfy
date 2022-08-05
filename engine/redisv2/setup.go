057f1cea2f348402b781194fb1951998d6ba9275
Pascal_Zhai <45193361+Napoleon0621@users.noreply.github.com>
2022-08-05 14:32:01 +0800

    Submit copied files for redisv2 version engine which remain unchanged compared with the original redis engine (#144)
    Co-authored-by: Pascal Zhai <Your Github Email>

---
+++ engine/redisv2/setup.go
@@ -0,0 +1,51 @@
+package redis
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/sirupsen/logrus"
+
+	"github.com/bitleak/lmstfy/config"
+	"github.com/bitleak/lmstfy/engine"
+	"github.com/bitleak/lmstfy/helper"
+)
+
+const MaxRedisConnections = 5000
+
+var (
+	logger   *logrus.Logger
+	dummyCtx = context.TODO()
+)
+
+// Setup set the essential config of redis engine
+func Setup(conf *config.Config, l *logrus.Logger) error {
+	logger = l
+	for name, poolConf := range conf.Pool {
+		if poolConf.PoolSize == 0 {
+			poolConf.PoolSize = MaxRedisConnections
+		}
+		opt := &redis.Options{}
+		// By Default, the timeout for RW is 3 seconds, we might get few error
+		// when redis server is doing AOF rewrite. We prefer data integrity over speed.
+		opt.ReadTimeout = 30 * time.Second
+		opt.WriteTimeout = 30 * time.Second
+		opt.MinIdleConns = 10
+		cli := helper.NewRedisClient(&poolConf, opt)
+		if cli.Ping(dummyCtx).Err() != nil {
+			return fmt.Errorf("redis server %s was not alive", poolConf.Addr)
+		}
+		e, err := NewEngine(name, cli)
+		if err != nil {
+			return fmt.Errorf("setup engine error: %s", err)
+		}
+		engine.Register(engine.KindRedis, name, e)
+	}
+	if engine.GetEngineByKind(engine.KindRedis, "") == nil {
+		return errors.New("default redis engine not found")
+	}
+	return nil
+}

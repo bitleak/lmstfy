90c05247b393406354667d5e50c10c8dc7ff226e
Pascal Zhai <Your Github Email>
2022-08-22 10:02:29 +0800

    spanner data manager adding loop pump function

--- storage/spanner/spanner.go
+++ storage/spanner/spanner.go
@@ -4,26 +4,44 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"time"
 
 	"cloud.google.com/go/spanner"
+	"github.com/go-redis/redis/v8"
+	"github.com/go-redsync/redsync/v4"
+	"github.com/go-redsync/redsync/v4/redis/goredis/v8"
+	log "github.com/sirupsen/logrus"
 
+	"github.com/bitleak/lmstfy/config"
+	"github.com/bitleak/lmstfy/engine"
+	"github.com/bitleak/lmstfy/storage"
 	"github.com/bitleak/lmstfy/storage/model"
 )
 
 const (
-	MaxAddJobBatchSize = 1000
+	MaxJobBatchSize       = 1000
+	DefaultLoopPumpPeriod = 10 * time.Second
+	LoopPumpLockName      = "LP_redsync"
 )
 
 type SpannerDataMgr struct {
-	cli       *spanner.Client
-	tableName string
+	cli         *spanner.Client
+	redisClient *redis.Client
+	tableName   string
+	shutdown    chan struct{}
 }
 
-func NewSpannerDataMgr(client *spanner.Client, tableName string) *SpannerDataMgr {
-	return &SpannerDataMgr{
-		cli:       client,
-		tableName: tableName,
+func NewDataMgr(cfg *config.SpannerConfig, redisClient *redis.Client) (storage.DataManager, error) {
+	client, err := CreateSpannerClient(cfg)
+	if err != nil {
+		return nil, err
 	}
+	return &SpannerDataMgr{
+		cli:         client,
+		redisClient: redisClient,
+		tableName:   cfg.TableName,
+		shutdown:    make(chan struct{}),
+	}, nil
 }
 
 // BatchAddJobs write jobs data into secondary storage
@@ -54,7 +72,7 @@ func (mgr *SpannerDataMgr) BatchGetJobs(ctx context.Context, req []*model.JobDat
 
 	for _, r := range req {
 		iter := txn.Query(ctx, spanner.Statement{
-			SQL: "SELECT job_id, namespace, queue, body, ready_time, expired_time, created_time " +
+			SQL: "SELECT job_id, namespace, queue, body, ready_time, expired_time, created_time, tries " +
 				"FROM lmstfy_jobs WHERE namespace = @namespace and queue = @queue and ready_time >= @readytime LIMIT @limit",
 			Params: map[string]interface{}{
 				"namespace": r.Namespace,
@@ -123,12 +141,108 @@ func (mgr *SpannerDataMgr) DelJobs(ctx context.Context, jobIDs []string) (count 
 	return count, err
 }
 
+// GetReadyJobs return jobs which are ready based on input ready time from data storage
+func (mgr *SpannerDataMgr) GetReadyJobs(ctx context.Context, req *model.JobDataReq) (jobs []*model.JobData, err error) {
+	if req.ReadyTime <= 0 {
+		return nil, fmt.Errorf("GetReadyJobs failed: missing readytime parameter")
+	}
+	txn := mgr.cli.ReadOnlyTransaction()
+	defer txn.Close()
+	iter := txn.Query(ctx, spanner.Statement{
+		SQL: "SELECT job_id, namespace, queue, body, ready_time, expired_time, created_time, tries " +
+			"FROM lmstfy_jobs WHERE ready_time <= @readytime and ready_time > @nowtime  LIMIT @limit",
+		Params: map[string]interface{}{
+			"readytime": req.ReadyTime,
+			"limit":     req.Count,
+			"nowtime":   time.Now().Unix(),
+		},
+	})
+	err = iter.Do(func(row *spanner.Row) error {
+		elem := &model.JobData{}
+		if err = row.ToStruct(elem); err != nil {
+			return err
+		}
+		jobs = append(jobs, elem)
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return jobs, nil
+}
+
+// LoopPump periodically checks job data ready time and pumps jobs to engine
+func (mgr *SpannerDataMgr) LoopPump(eng engine.Engine) {
+	fmt.Printf("enter loop pump at %v\n", time.Now())
+	tick := time.NewTicker(DefaultLoopPumpPeriod)
+	//mutex := newLoopPumpMutex(mgr.redisClient)
+	pool := goredis.NewPool(mgr.redisClient)
+	rs := redsync.New(pool)
+	mutex := rs.NewMutex(LoopPumpLockName)
+	for {
+		select {
+		case now := <-tick.C:
+			fmt.Printf("start process loop pump at %v\n", time.Now())
+			ctx := context.Background()
+			if err := mutex.LockContext(ctx); err != nil {
+				fmt.Printf("LoopPump failed to acquire lock: %v\n", err)
+				log.Errorf("LoopPump failed to acquire lock: %v", err)
+				continue
+			}
+
+			req := &model.JobDataReq{
+				ReadyTime: now.Unix() + int64(DefaultLoopPumpPeriod),
+				Count:     MaxJobBatchSize,
+			}
+			jobs, err := mgr.GetReadyJobs(ctx, req)
+			if err != nil {
+				log.Errorf("LoopPump failed to GetReadyJobs: %v", err)
+				continue
+			}
+			jobsID := make([]string, 0)
+			for _, j := range jobs {
+				_, err := eng.Publish(j.Namespace, j.Queue, j.Body, uint32(j.ExpiredTime),
+					uint32(j.ReadyTime-now.Unix()), uint16(j.Tries))
+				if err != nil {
+					log.Errorf("LoopPump failed to publish job:%v with error %v", j.JobID, err)
+					continue
+				}
+				jobsID = append(jobsID, j.JobID)
+			}
+
+			_, err = mgr.DelJobs(ctx, jobsID)
+			if err != nil {
+				log.Errorf("LoopPump delete jobs failed:%v", err)
+			}
+			time.Sleep(5 * time.Second)
+			if ok, err := mutex.UnlockContext(ctx); !ok || err != nil {
+				log.Errorf("LoopPump failed to release lock: %v", err)
+				continue
+			}
+			fmt.Printf("end process loop pump at %v\n", time.Now())
+		case <-mgr.shutdown:
+			return
+		}
+	}
+}
+
+func (mgr *SpannerDataMgr) ShutDown() {
+	close(mgr.shutdown)
+}
+
 func validateReq(req []*model.JobData) error {
 	if len(req) == 0 {
 		return errors.New("invalid req, null jobs list")
 	}
-	if len(req) > MaxAddJobBatchSize {
+	if len(req) > MaxJobBatchSize {
 		return errors.New("invalid req, exceed maximum input batch size")
 	}
 	return nil
+}
+
+func newLoopPumpMutex(client *redis.Client) *redsync.Mutex {
+	pool := goredis.NewPool(client)
+	rs := redsync.New(pool)
+	mutex := rs.NewMutex("LoopPump-redsync")
+	return mutex
 }
--- storage/spanner/spanner_test.go
+++ storage/spanner/spanner_test.go
@@ -1,183 +1,27 @@
 package spanner
 
 import (
-	"context"
 	"fmt"
 	"os"
-	"regexp"
 	"testing"
-	"time"
 
-	"cloud.google.com/go/spanner"
-	database "cloud.google.com/go/spanner/admin/database/apiv1"
-	instance "cloud.google.com/go/spanner/admin/instance/apiv1"
 	"github.com/stretchr/testify/assert"
-	databasepb "google.golang.org/genproto/googleapis/spanner/admin/database/v1"
-	instancepb "google.golang.org/genproto/googleapis/spanner/admin/instance/v1"
-	"google.golang.org/grpc/codes"
-
-	"github.com/bitleak/lmstfy/storage/conf"
-	"github.com/bitleak/lmstfy/storage/model"
 )
 
-var cfg = &conf.SpannerConfig{
-	Project:  "test-project",
-	Instance: "test-instance",
-	Database: "test-db",
-}
-
-var (
-	tableName = "lmstfy_jobs"
-	jobIDs    = []string{"1", "2", "3"}
-	ctx       = context.Background()
-	db        = "projects/test-project/instances/test-instance/databases/test-db"
-)
+var db = "projects/test-project/instances/test-instance/databases/test-db"
 
 func init() {
 	if os.Getenv("SPANNER_EMULATOR_HOST") == "" {
-		fmt.Println("failed to find $SPANNER_EMULATOR_HOST value")
-		return
+		panic(fmt.Sprintf("failed to find $SPANNER_EMULATOR_HOST value"))
 	}
-	err := createInstance(ctx, db)
+	err := CreateInstance(dummyCtx, db)
 	if err != nil {
-		fmt.Printf("create instance with error: %v", err)
-		return
+		panic(fmt.Sprintf("create instance error: %v", err))
 	}
-	err = createDatabase(ctx, db)
+	err = CreateDatabase(dummyCtx, db)
 	if err != nil {
-		fmt.Printf("create db with error: %v", err)
-		return
+		panic(fmt.Sprintf("create db error: %v", err))
 	}
-}
-
-func createInstance(ctx context.Context, uri string) error {
-	matches := regexp.MustCompile("projects/(.*)/instances/(.*)/databases/.*").FindStringSubmatch(uri)
-	if matches == nil || len(matches) != 3 {
-		return fmt.Errorf("invalid instance id %s", uri)
-	}
-	instanceName := "projects/" + matches[1] + "/instances/" + matches[2]
-
-	instanceAdminClient, err := instance.NewInstanceAdminClient(ctx)
-	if err != nil {
-		return err
-	}
-	defer instanceAdminClient.Close()
-
-	_, err = instanceAdminClient.GetInstance(ctx, &instancepb.GetInstanceRequest{
-		Name: instanceName,
-	})
-	if err != nil && spanner.ErrCode(err) != codes.NotFound {
-		return err
-	}
-	if err == nil {
-		return nil
-	}
-
-	_, err = instanceAdminClient.CreateInstance(ctx, &instancepb.CreateInstanceRequest{
-		Parent:     "projects/" + matches[1],
-		InstanceId: matches[2],
-	})
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func createDatabase(ctx context.Context, uri string) error {
-	matches := regexp.MustCompile("^(.*)/databases/(.*)$").FindStringSubmatch(uri)
-	if matches == nil || len(matches) != 3 {
-		return fmt.Errorf("invalid database id %s", uri)
-	}
-
-	databaseAdminClient, err := database.NewDatabaseAdminClient(ctx)
-	if err != nil {
-		return err
-	}
-	_, err = databaseAdminClient.GetDatabase(ctx, &databasepb.GetDatabaseRequest{Name: uri})
-	if err != nil && spanner.ErrCode(err) != codes.NotFound {
-		return err
-	}
-	if err == nil {
-		// db exists
-		return nil
-	}
-
-	op, err := databaseAdminClient.CreateDatabase(ctx, &databasepb.CreateDatabaseRequest{
-		Parent:          matches[1],
-		CreateStatement: "CREATE DATABASE `" + matches[2] + "`",
-		ExtraStatements: []string{
-			`CREATE TABLE lmstfy_jobs (
-								job_id       STRING(1024),
-								namespace    STRING(1024),
-								queue        STRING(1024),
-								body         BYTES(MAX),
-								expired_time INT64 NOT NULL,
-								ready_time   INT64 NOT NULL,
-								tries        INT64 NOT NULL,
-                                created_time  INT64 NOT NULL
-			                       ) PRIMARY KEY (job_id)`,
-		},
-	})
-	if err != nil {
-		return err
-	}
-	if _, err = op.Wait(ctx); err != nil {
-		return err
-	}
-	return nil
-}
-func createTestJobsData() []*model.JobData {
-	jobs := make([]*model.JobData, 0)
-	j1 := &model.JobData{
-		JobID:       "1",
-		Namespace:   "n1",
-		Queue:       "q1",
-		Body:        nil,
-		ExpiredTime: 1,
-		ReadyTime:   1,
-		Tries:       1,
-		CreatedTime: time.Now().Unix(),
-	}
-	j2 := &model.JobData{
-		JobID:       "2",
-		Namespace:   "n1",
-		Queue:       "q2",
-		Body:        nil,
-		ExpiredTime: 1,
-		ReadyTime:   1,
-		Tries:       1,
-		CreatedTime: time.Now().Unix(),
-	}
-	j3 := &model.JobData{
-		JobID:       "3",
-		Namespace:   "n1",
-		Queue:       "q1",
-		Body:        nil,
-		ExpiredTime: 1,
-		ReadyTime:   1,
-		Tries:       1,
-		CreatedTime: time.Now().Unix(),
-	}
-	jobs = append(jobs, j1, j2, j3)
-	return jobs
-}
-
-func createTestReqData() []*model.JobDataReq {
-	req := make([]*model.JobDataReq, 0)
-	r1 := &model.JobDataReq{
-		Namespace: "n1",
-		Queue:     "q1",
-		ReadyTime: 0,
-		Count:     10,
-	}
-	r2 := &model.JobDataReq{
-		Namespace: "n1",
-		Queue:     "q2",
-		ReadyTime: 0,
-		Count:     10,
-	}
-	req = append(req, r1, r2)
-	return req
 }
 
 func TestCreateSpannerClient(t *testing.T) {
@@ -186,11 +30,11 @@ func TestCreateSpannerClient(t *testing.T) {
 }
 
 func TestSpannerDataMgr_BatchAddDelJobs(t *testing.T) {
-	cli, err := CreateSpannerClient(cfg)
+	mgr, err := NewDataMgr(cfg, R.Conn)
 	if err != nil {
 		panic(fmt.Sprintf("Failed to create spanner client with error: %s", err))
 	}
-	mgr := NewSpannerDataMgr(cli, tableName)
+	defer mgr.ShutDown()
 	jobs := createTestJobsData()
 	err = mgr.BatchAddJobs(ctx, jobs)
 	if err != nil {
@@ -206,11 +50,11 @@ func TestSpannerDataMgr_BatchAddDelJobs(t *testing.T) {
 }
 
 func TestSpannerDataMgr_BatchGetJobs(t *testing.T) {
-	cli, err := CreateSpannerClient(cfg)
+	mgr, err := NewDataMgr(cfg, R.Conn)
 	if err != nil {
 		panic(fmt.Sprintf("Failed to create spanner client with error: %s", err))
 	}
-	mgr := NewSpannerDataMgr(cli, tableName)
+	defer mgr.ShutDown()
 	jobs := createTestJobsData()
 	mgr.BatchAddJobs(ctx, jobs)
 	req := createTestReqData()
@@ -222,11 +66,11 @@ func TestSpannerDataMgr_BatchGetJobs(t *testing.T) {
 }
 
 func TestSpannerDataMgr_GetQueueSize(t *testing.T) {
-	cli, err := CreateSpannerClient(cfg)
+	mgr, err := NewDataMgr(cfg, R.Conn)
 	if err != nil {
 		panic(fmt.Sprintf("Failed to create spanner client with error: %s", err))
 	}
-	mgr := NewSpannerDataMgr(cli, tableName)
+	defer mgr.ShutDown()
 	jobs := createTestJobsData()
 	mgr.BatchAddJobs(ctx, jobs)
 	req := createTestReqData()

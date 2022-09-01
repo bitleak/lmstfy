ff0e337b17a2293697dad4b330f5300e60cb0a27
Pascal_Zhai <45193361+Napoleon0621@users.noreply.github.com>
2022-09-01 14:13:21 +0800

    Add monitor meterics for secondary storage (#174)
    * add monitor metrics for secondary storage
    
    * fix metrics name and remove redundant code
    
    * add namespace and queue as metric's label
    
    * put storage addjob status to metrics label
    
    Co-authored-by: Pascal Zhai <Your Github Email>

---
+++ storage/metrics.go
@@ -0,0 +1,44 @@
+package storage
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+// Metrics contains storage related metrics
+type Metrics struct {
+	storageAddJobs *prometheus.CounterVec
+	storageDelJobs *prometheus.CounterVec
+}
+
+var (
+	metrics *Metrics
+)
+
+const (
+	Namespace = "infra"
+	Subsystem = "lmstfy_v2_storage"
+)
+
+func setupMetrics() {
+	cv := newCounterVecHelper
+	metrics = &Metrics{
+		storageAddJobs: cv("storage_add_jobs", "namespace", "queue", "status"),
+		storageDelJobs: cv("storage_del_jobs"),
+	}
+}
+
+func newCounterVecHelper(name string, labels ...string) *prometheus.CounterVec {
+	labels = append([]string{"pool"}, labels...) // all metrics has this common field `pool`
+	opts := prometheus.CounterOpts{}
+	opts.Namespace = Namespace
+	opts.Subsystem = Subsystem
+	opts.Name = name
+	opts.Help = name
+	counters := prometheus.NewCounterVec(opts, labels)
+	prometheus.MustRegister(counters)
+	return counters
+}
+
+func init() {
+	setupMetrics()
+}

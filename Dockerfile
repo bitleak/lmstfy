44ecf23f2f2bf0b26c08dae90ac921f002168918
hulk <hulk.website@gmail.com>
2023-02-23 20:58:47 +0800

    Use GitHub Actions to release the docker image

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
-FROM golang:1.15.6
+FROM golang:1.17
 
 WORKDIR /lmstfy
+
 ADD ./ /lmstfy
 RUN apt update -y && apt install -y netcat
-RUN cd /lmstfy && make
+RUN cd /lmstfy && make TARGET_OS=$TARGET_OS TARGET_ARCH=$TARGET_ARCH
 EXPOSE 7777:7777
 ENTRYPOINT ["/lmstfy/_build/lmstfy-server", "-c", "/lmstfy/config/docker-image-conf.toml"]
 
--- build.sh
+++ build.sh
@@ -1,56 +1,5 @@
 #!/bin/bash
 set -e
-if test -z "$TARGET_OS"; then
-    uname_S=`uname -s`
-    case "$uname_S" in
-        Darwin)
-            TARGET_OS=darwin
-            ;;
-        OpenBSD)
-            TARGET_OS=openbsd
-            ;;
-        DragonFly)
-            TARGET_OS=dragonfly
-            ;;
-        FreeBSD)
-            TARGET_OS=freebsd
-            ;;
-        NetBSD)
-            TARGET_OS=netbsd
-            ;;
-        SunOS)
-            TARGET_OS=solaris
-            ;;
-        *)
-            TARGET_OS=linux
-            ;;
-    esac
-fi
-
-if test -z "$TARGET_ARCH"; then
-    uname_M=`uname -m`
-    case "$uname_M" in
-        x86_64)
-            TARGET_ARCH=amd64
-            ;;
-        arm*)
-            TARGET_ARCH=arm64
-            ;;
-        ppc64*)
-            TARGET_ARCH=ppc64
-            ;;
-        aarch64)
-            TARGET_ARCH=arm
-            ;;
-        i386)
-            TARGET_ARCH=386
-            ;;
-        *)
-            TARGET_ARCH=amd64
-            ;;
-    esac
-fi
-
 TARGET_NAME=lmstfy-server
 GO_PROJECT=github.com/bitleak/lmstfy
 BUILD_DIR=./_build
@@ -58,7 +7,7 @@ VERSION=`grep "^VERSION" Changelog | head -1 | cut -d " " -f2`
 BUILD_DATE=`date -u +'%Y-%m-%dT%H:%M:%SZ'`
 GIT_REVISION=`git rev-parse --short HEAD`
 
-GOOS="$TARGET_OS" GOARCH="$TARGET_ARCH" go build -v -ldflags \
+go build -v -ldflags \
     "-X $GO_PROJECT/version.Version=$VERSION -X $GO_PROJECT/version.BuildDate=$BUILD_DATE -X $GO_PROJECT/version.BuildCommit=$GIT_REVISION" \
     -o ${TARGET_NAME} ${GO_PROJECT}/server
 if [[ $? -ne 0 ]]; then
